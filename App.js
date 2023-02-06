be0f02c9a10481a38f629b0e2c27a522a6fef2d0
prikmmo9 <109761180+prikmmo9@users.noreply.github.com>
2023-02-07 00:31:20 +0300

    Update App.js

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="App">
    work
-        <button onClick={onClose}>Закрыть</button>
+        <button onClick={onClose}>Закрыть2</button>
     </div>
   );
 }
